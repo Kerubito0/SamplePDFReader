37456cf7dd8aec3f67211a5c217709439f00c568
TMS <munbai6666@gmail.com>
2017-05-31 10:24:28 +0900

    Initial Commit

---
+++ PDFReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PDFReader
+//
+//  Created by 谷川ばい on 2017/05/31.
+//  Copyright © 2017年 TMS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
